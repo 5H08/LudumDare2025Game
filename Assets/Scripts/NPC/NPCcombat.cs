f7942dad81f05b6c593a66c0b6415ddb2af7d891
5H08 <esuanomaly69@gmail.com>
2025-10-04 02:40:50 -0600

    More Combat Update

--- Assets/Scripts/NPC/NPCcombat.cs
+++ Assets/Scripts/NPC/NPCcombat.cs
@@ -5,6 +5,7 @@ public class NPCcombat : MonoBehaviour
     
     public void ReceiveHit(Transform player)
     {
-
+        // Fly out against player's position
+        // Pause navigation for a few seconds
     }
 }
