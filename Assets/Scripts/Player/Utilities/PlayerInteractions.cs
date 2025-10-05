8343e34d100a066ae92f6c0bab854ffef6f753c7
5H08 <esuanomaly69@gmail.com>
2025-10-05 06:31:25 -0600

    Weird
    huh huh

---
+++ Assets/Scripts/Player/Utilities/PlayerInteractions.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    public List items = new List();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ItemPickup()
+    {
+
+    }
+}
