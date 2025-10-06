b58c4b561d7409dc51f1c8492f3ee67dd0fcc56d
5H08 <esuanomaly69@gmail.com>
2025-10-06 05:37:03 -0600

    goodstuff

--- Assets/Scripts/Player/Utilities/PlayerCombat.cs
+++ Assets/Scripts/Player/Utilities/PlayerCombat.cs
@@ -3,44 +3,87 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
-    private Coroutine punchRoutine = null;
     [SerializeField] private GameObject trigger;
+    [SerializeField] private Animator leftAnim;
+    [SerializeField] private Animator rightAnim;
+
+    private Coroutine punchRoutine = null;
+    private bool canLeft = true;
+    private bool canRight = true;
 
     void Start()
     {
         trigger = transform.GetChild(1).gameObject;
+        GameObject canv = GameObject.FindGameObjectWithTag("UI Canvas");
+        leftAnim = canv.transform.GetChild(0).GetChild(0).GetComponent<Animator>();
+        rightAnim = canv.transform.GetChild(0).GetChild(1).GetComponent<Animator>();
     }
 
     public void LeftPunch()
     {
-        if (punchRoutine != null) 
+        if (canLeft)
         {
-            StopCoroutine(punchRoutine);
-            punchRoutine = StartCoroutine(PunchCoroutine());
+            StartCoroutine(LeftCooldown());
+            leftAnim.SetBool("Punching", true);
+            if (punchRoutine != null)
+            {
+                StopCoroutine(punchRoutine);
+                punchRoutine = StartCoroutine(PunchCoroutine());
+            }
+            else
+            {
+                punchRoutine = StartCoroutine(PunchCoroutine());
+            }
         }
         else
         {
-            punchRoutine = StartCoroutine(PunchCoroutine());
+
         }
     }
 
     public void RightPunch()
     {
-        if (punchRoutine != null)
+        if (canRight)
         {
-            StopCoroutine(punchRoutine);
-            punchRoutine = StartCoroutine(PunchCoroutine());
+            StartCoroutine(RightCooldown());
+            rightAnim.SetBool("Punching", true);
+            if (punchRoutine != null)
+            {
+                StopCoroutine(punchRoutine);
+                punchRoutine = StartCoroutine(PunchCoroutine());
+            }
+            else
+            {
+                punchRoutine = StartCoroutine(PunchCoroutine());
+            }
         }
         else
         {
-            punchRoutine = StartCoroutine(PunchCoroutine());
+
         }
+        
+    }
+
+    IEnumerator LeftCooldown()
+    {
+        canLeft = false;
+        yield return new WaitForSeconds(5);
+        canLeft = true;
+    }
+
+    IEnumerator RightCooldown()
+    {
+        canRight = false;
+        yield return new WaitForSeconds(5);
+        canRight = true;
     }
 
     IEnumerator PunchCoroutine()
     {
         trigger.SetActive(false);
         yield return new WaitForFixedUpdate();
+        leftAnim.SetBool("Punching", false);
+        rightAnim.SetBool("Punching", false);
         trigger.SetActive(true);
         yield return new WaitForSeconds(.5f);
         trigger.SetActive(false);
